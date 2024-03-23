dfd17da9b0f8da475f843544ae9d182c7bb90517
Euphyllia <bierque.euphyllia@gmail.com>
2024-03-23 14:32:51 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.logging.log4j:log4j-api:2.22.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.22.1")
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.3.2")
-    implementation("com.zaxxer:HikariCP:5.1.0")
+    compileOnly("org.apache.logging.log4j:log4j-api:2.22.1")
+    compileOnly("org.apache.logging.log4j:log4j-core:2.22.1")
+    compileOnly("org.mariadb.jdbc:mariadb-java-client:3.3.2")
+    compileOnly("com.zaxxer:HikariCP:5.1.0")
     implementation("org.jetbrains:annotations:24.0.0")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
